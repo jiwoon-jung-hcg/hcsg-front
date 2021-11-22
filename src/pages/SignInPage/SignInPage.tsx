094689600993f59db11f36d5992124c5ea477ec4
JIUN JEONG <jiunjeong@JIUNui-MacBookPro.local>
2021-11-22 13:32:56 +0900

    feat: Login Componenet Layout

--- src/pages/SignInPage/SignInPage.tsx
+++ src/pages/SignInPage/SignInPage.tsx
@@ -1,122 +1,125 @@
 import React, { useCallback, useState } from 'react';
-import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
 const useStyles = makeStyles((theme) => ({
-  container: {
-    height: '100vh',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
+	container: {
+		height: '100vh',
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	paper: {
+		marginTop: theme.spacing(8),
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+	},
+	avatar: {
+		margin: theme.spacing(1),
+		backgroundColor: theme.palette.secondary.main,
+	},
+	form: {
+		width: '100%', // Fix IE 11 issue.
+		marginTop: theme.spacing(1),
+	},
+	submit: {
+		margin: theme.spacing(3, 0, 2),
+	},
 }));
 
 export default function SignInPage() {
-  const classes = useStyles();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+	const classes = useStyles();
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 
-  const handleInputEmail = useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>) => {
-      setEmail(event.currentTarget.value);
-    },
-    [email],
-  );
+	const handleInputEmail = useCallback(
+		(event: React.ChangeEvent<HTMLInputElement>) => {
+			setEmail(event.currentTarget.value);
+		},
+		[email],
+	);
 
-  const handleInputPassword = useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>) => {
-      setPassword(event.currentTarget.value);
-    },
-    [password],
-  );
+	const handleInputPassword = useCallback(
+		(event: React.ChangeEvent<HTMLInputElement>) => {
+			setPassword(event.currentTarget.value);
+		},
+		[password],
+	);
 
-  return (
-    <div className={classes.container}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <div className={classes.paper}>
-          <Typography component="h1" variant="h5">
-            <img src="images/logo2.png" alt="logo" className="" />
-          </Typography>
-          <form className={classes.form} noValidate>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="이메일"
-              name="email"
-              value={email}
-              autoComplete="email"
-              autoFocus
-              onChange={handleInputEmail}
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="비밀번호"
-              type="password"
-              id="password"
-              value={password}
-              autoComplete="current-password"
-              onChange={handleInputPassword}
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              className={classes.submit}
-              style={{ backgroundColor: '#5ea0a2', color: 'white' }}
-            >
-              로그인
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2" style={{ color: '#63A0BA' }}>
-                  패스워드 찾기
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2" style={{ color: '#63A0BA' }}>
-                  회원가입
-                </Link>
-              </Grid>
-            </Grid>
-          </form>
-        </div>
-      </Container>
-    </div>
-  );
+	const handleFormSubmit = useCallback(
+		(event) => {
+			event.preventDefault();
+		},
+		[email, password],
+	);
+
+	return (
+		<div className={classes.container}>
+			<Container component="main" maxWidth="xs">
+				<CssBaseline />
+				<div className={classes.paper}>
+					<Typography component="h1" variant="h5">
+						<img src="images/logo2.png" alt="logo" className="" />
+					</Typography>
+					<form className={classes.form} onSubmit={handleFormSubmit}>
+						<TextField
+							type="email"
+							variant="outlined"
+							margin="normal"
+							required
+							fullWidth
+							id="email"
+							label="이메일"
+							name="email"
+							value={email}
+							autoComplete="email"
+							autoFocus
+							onChange={handleInputEmail}
+						/>
+						<TextField
+							variant="outlined"
+							margin="normal"
+							required
+							fullWidth
+							name="password"
+							label="비밀번호"
+							type="password"
+							id="password"
+							value={password}
+							autoComplete="current-password"
+							onChange={handleInputPassword}
+						/>
+						<Button
+							type="submit"
+							fullWidth
+							variant="contained"
+							className={classes.submit}
+							style={{ backgroundColor: '#5ea0a2', color: 'white' }}
+						>
+							로그인
+						</Button>
+						<Grid container>
+							<Grid item xs>
+								<Link href="#" variant="body2" style={{ color: '#63A0BA' }}>
+									패스워드 찾기
+								</Link>
+							</Grid>
+							<Grid item>
+								<Link href="#" variant="body2" style={{ color: '#63A0BA' }}>
+									회원가입
+								</Link>
+							</Grid>
+						</Grid>
+					</form>
+				</div>
+			</Container>
+		</div>
+	);
 }
