2fca17bf55dd2702ac3b2e8f5a80dd7d98508960
JIUN JEONG <jiunjeong@JIUNui-MacBookPro.local>
2021-11-26 01:01:40 +0900

    feat: 로그인 회원가입 로직구현
    - 로그인 요청 구현
    - 로그인 피드백구현(아이디, 비밀번호 체크)
    - 회원가입 요청 구현
    - 회원가입 중복 닉네임, 이메일 체크
    - 회원가입 벨리데이션 체크
    - 회원가입 피드백 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import * as React from 'react';
-import SignInPage from './pages/SignInPage/SignInPage';
-import SignUpPage from './pages/SignUpPage/SignUpPage';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import loadable from '@loadable/component';
+import { Routes, Route } from 'react-router-dom';
+const SignInPage = loadable(() => import('./pages/SignInPage/SignInPage'));
+const SignUpPage = loadable(() => import('./pages/SignUpPage/SignUpPage'));
 
 const App = () => {
 	return (
-		<Router>
-			<Routes>
-				<Route path="/signin" element={<SignInPage />} />
-				<Route path="/signup" element={<SignUpPage />} />
-				{/* <Route path="/" element={<Home />} /> */}
-			</Routes>
-		</Router>
+		<Routes>
+			<Route path="/signin" element={<SignInPage />} />
+			<Route path="/signup" element={<SignUpPage />} />
+			{/* <Route path="/" element={<Home />} /> */}
+		</Routes>
 	);
 };
 
