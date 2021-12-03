11f69a9b2ee818253daf742829c14ec78d77a458
jiwoon-jung-hcg <93896384+jiwoon-jung-hcg@users.noreply.github.com>
2021-12-03 14:26:14 +0900

    feat/auth : 라우팅 인증체크
    - 라우팅시 인증 체크 구현
    1. 로그인 유지
    2. 유효한 토큰 확인
    3. 로그인 유무예따른 페이지 접근 처리

--- src/hoc/authCheck.tsx
+++ src/hoc/authCheck.tsx
@@ -1,13 +1,50 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
+import { headerConfig } from '../apis/signUp/signUp';
+import { logger } from '../utils/logger';
+import { useNavigate, useParams } from 'react-router-dom';
 
-function authCheck(SpecificComponent: any, option = false) {
+/** Autorization 체크 */
+/** 1. 로그인 유지 */
+/** 2. 유효한 토큰 확인 */
+/** 3. 로그인 유무예따른 페이지 접근 처리 */
+/**
+ * SpecificComponent -> 도달하고자하는 컴포넌트
+ * option: 도달하고자 하는 컴포넌트에 로그인유무에 따른 필터 조건
+ *      -> 1 : 로그인된유저만 갈 수 있는 페이지
+ *      -> 0 : 모든 유저가 갈 수 있는 페이지
+ *      -> -1 : 로그인된 유저는 갈 수없는 페이지
+ * */
+function authCheck(SpecificComponent: any, option: 1 | 0 | -1 = 0) {
 	function AuthenticationCheck() {
+		const params = useParams();
+		console.log(params);
+		const navigate = useNavigate();
 		useEffect(() => {
-			console.log('dd');
+			axios(`${process.env.REACT_APP_SERVER_URL}/api/v1/auth_check`, headerConfig)
+				.then((response) => {
+					console.log(response);
+					const { isAuth, nickname }: { isAuth: boolean; nickname: string } = response.data;
+					// 토큰이 정상인증이 되었지만 로그인 안한 유저만 접근이가능할 떄 => 홈페이지로 리다이렉팅
+					if (isAuth && option === -1) {
+						return navigate('/');
+					}
+					// 토큰이 정상적이지 않지만 로그인된 유저만 들어가야될때 => 로그인페이지로 리다이렉팅
+					else if (!isAuth && option === 1) {
+						return navigate('/signin');
+					}
+				})
+				.catch((error) => {
+					logger(error);
+					console.dir(error);
+					navigate('/');
+				});
 		}, []);
+
 		return <SpecificComponent />;
 	}
 
 	return AuthenticationCheck;
 }
+
+export default authCheck;
